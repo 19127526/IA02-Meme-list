3a763ffb12435caaab3648c99408fbd79859f23d
Your Name <phamtienquan2001@gmail.com>
2022-11-09 22:49:00 +0700

    done

---
+++ src/components/Notification/Notification.js
@@ -0,0 +1,12 @@
+import {notification} from 'antd';
+
+
+const Notification=(title,description)=>{
+  return notification.success({
+    message: title,
+    description:description,
+  });
+}
+
+export default Notification
+
--- src/pages/click/ClickPage.jsx
+++ src/pages/click/ClickPage.jsx
@@ -3,9 +3,9 @@ import {useEffect, useState} from "react";
 import {getMemeApi} from "../../apis/MemeApi/MemeApi";
 import CardComponent from "../../components/card/CardComponent";
 import {Col, Container, Row} from "react-bootstrap";
-import styles from './ClickPage.module.css'
-import Trail from "./Trail";
-
+import { PoweroffOutlined } from '@ant-design/icons';
+import { Button, Space } from 'antd';
+import Notification from "../../components/Notification/Notification";
 const pageIndex = 6;
 
 const ClickPage = () => {
@@ -14,12 +14,15 @@ const ClickPage = () => {
   const [page, setPage] = useState(1);
   const currentIndexPage = pageIndex * page;
   const prevIndexPage = pageIndex * (page - 1);
-
+  const [loading,setLoading]=useState(false);
 
   useEffect(() => {
     if (click === false) return;
+    setLoading(true);
     getMemeApi().then(async data => {
-      setData(data.data.memes)
+      setData(data.data.memes);
+      setLoading(false)
+      Notification("Load data success")
     })
   }, [click]);
 
@@ -27,31 +30,31 @@ const ClickPage = () => {
     <Container>
       <Row>
         <Col xs={12} sm={12} lg={12} md={12} xl={12} style={{display: "flex", justifyContent: "center"}}>
-          <button type="primary" onClick={() => setClick(true)}>
-            Click me
-          </button>
+          <Button
+            type="primary"
+            icon={<PoweroffOutlined />}
+            style={{background:"#001529",border:"solid #001529",marginTop:"10px"}}
+            loading={loading}
+            onClick={() => setClick(true)}
+          >
+            Click me!
+          </Button>
         </Col>
       </Row>
-
-
       {click === true && data.length > 0 ?
         <>
-         {/* <div className={styles.container}>*/}
-            <Row>
-             {/* <Trail open={click}>*/}
-                {data &&
-                  data?.map((user, index) => {
-                      return prevIndexPage <= index && index < currentIndexPage ?
-                        <Col xs={12} sm={12} lg={4} md={6} xl={4} key={user.id}>
-                          <CardComponent name={user.name} img={user.url} id={user.id}/>
-                        </Col>
-                        : ""
-                    }
-                  )
+          <Row>
+            {data &&
+              data?.map((user, index) => {
+                  return prevIndexPage <= index && index < currentIndexPage ?
+                    <Col xs={12} sm={12} lg={4} md={6} xl={4} key={user.id}>
+                      <CardComponent name={user.name} img={user.url} id={user.id}/>
+                    </Col>
+                    : ""
                 }
-              {/*</Trail>*/}
-            </Row>
-         {/* </div>*/}
+              )
+            }
+          </Row>
           <Row className="m-2">
             <Col xs={12} sm={12} lg={12} md={12} xl={12} style={{display: "flex", justifyContent: "center"}}>
               <Pagination
@@ -69,8 +72,6 @@ const ClickPage = () => {
         : ""
       }
     </Container>
-
   )
 }
-
 export default ClickPage
